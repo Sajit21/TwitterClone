b850105c5af7b12974121a0f0fa8327e3b5e9b69
Sajit Maharjan <sajitmhr13@gmail.com>
2025-01-06 15:56:15 +0545

    feat:- add of signup and login

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,13 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs"
+import { generateTokenAndSetCookie } from "../lib/generateToken.js";
 
 
 export const signUp=async(req, res) => {
    try {
 
     const {fullname,username,password,email} =req.body;
+    console.log(fullname,username,password,email)
     const emailregex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(!(emailregex).test(email))
     {
@@ -35,38 +37,96 @@ export const signUp=async(req, res) => {
     const salt=await bcrypt.genSalt(10)
 
     const hashpassword= await bcrypt.hash(password,salt)
-
-    //after the conent the user is created// define all the content
+//    console.log("im ruuning this hash")
+    //after the content the user is created// define all the content
+    //creating a user
+    console.log(hashpassword,password,fullname,username)
     const newUser = new User({
         fullname,
         username,
         password:hashpassword,
         email
     })
+    console.log(newUser)
 
 
     //after the create of user . cookie and token is gernerated 
     if(newUser){
         generateTokenAndSetCookie(newUser._id,res)
-       await newUser.save(); // saving the content of the newUser acc to their id 
+        await newUser.save(); // saving the content of the newUser acc to their id 
+    
+   
+    res.status(201).json({
+        _id:newUser._id,
+        fullName: newUser.fullname,
+        username:newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg
+
+    })
+}
+
+    else
+    {
+        res.status(400).json({message: "invalid data of user"})
     }
 
-
-
-   } catch (error) {
+} catch (error) {
+    console.log("invalid error of the data ")
+    res.status(400).json({message: "interna server error "})
     
    }
-res.status(200).json({message: "sigup is connected"})
+// res.status(200).json({message: "sigup is connected"})
 
 }
-// export const login=async(req, res) => {
-//     try {
-//      const {}
-//     } catch (error) {
+
+
+
+//incase of login :
+//ask for username and password then find one username, check for the password,then generate token
+
+export const login=async(req, res) => {
+     try {
+
+        const {username,password}=req.body;
+        const user=await User.findOne({username})
+        const isPasswordCorrect=await bcrypt.compare(password, user?.password || "")
+        if(!user || !isPasswordCorrect)
+        {
+            res.status(400).json({message:"invalid username and password"})
+        }
+
+        //if username ra password milyo bhani 
+        generateTokenAndSetCookie(user._id,res)
+
+        //display them in postman
+        res.status(201).json({
+            _id:user._id,
+            fullName: user.fullname,
+            username:user.username,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg
+    
+        })
+
+
+   //If valid, you generate a new authentication token and add it to the cookie.
+
+   } catch (error) {
+    console.log("invalid error in the server")
+    res.status(400).json({message:"error found in the server"})
      
-//     }
+    }
  
-//  }
+ }
+
+
 //  export const logout=async(req, res) => {
 //     try {
 //      const {}
---
+++ backend/lib/generateToken.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+
+
+// export const generateTokenAndSetCookie=(userId,res)=>{
+   
+//     try {
+//         const token =jwt.sign({userId},process.env.JWT_SECRET,{
+//             expiresIn: "15d",
+//         })
+
+
+//         res.cookie("jwt",token,{
+//             maxAge: 15*24* 60*60*1000,
+//             httpOnly:true,
+//             sameSite:"strict",
+//             secure:process.env.NOD_ENV !== "development" }) //only secure is true when it is false
+        
+//     } catch (error) {
+        
+//     }
+// }
+
+export const generateTokenAndSetCookie=(userId,res)=>{
+    const token =jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn: "15d",
+    })
+
+
+    res.cookie("jwt",token,{
+        maxAge: 15*24* 60*60*1000,
+        httpOnly:true,
+        sameSite:"strict",
+        secure:process.env.NOD_ENV !== "development" }) //only secure is true when it is false
+    
+
+}
